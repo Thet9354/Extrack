f52f59fec42964a69b6486955b0a3ebf2a5406b3
PhoonThetPine <thetpine254@gmail.com>
2023-04-05 19:29:56 +0800

    Initial Commit

---
+++ ExTrack/ExTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExTrackApp.swift
+//  ExTrack
+//
+//  Created by Phoon Thet Pine on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ExTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
